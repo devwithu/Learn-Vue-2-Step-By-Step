9a73effb0a3fca677d4e3656f4664af7f3087a3f
nohack <nohack@base2>
2017-11-04 09:28:39 +0900

    Episod 7

--- main.js
+++ main.js
@@ -2,6 +2,25 @@ Vue.component('task', {
 	template: '<li><slot></slot></li>'
 });
 
+Vue.component('task-list', {
+	template: `
+				<div>
+				    <task v-for="task in tasks">{{ task.task }}</task>
+				</div>
+			`,
+	data() {
+		return {
+			tasks: [
+				{task: 'sfsfsfssf', complete:true},
+				{task: 'sfsfsfssf', complete:false},
+				{task: 'sfsfsfssf', complete:true},
+				{task: 'sfsfsfssf', complete:true},
+
+			]
+		}
+	}
+});
+
 new Vue({
 	el: '#root'
 });
