6bee033084cf75ebbaf73afd90142914a358bde1
nohack <nohack@base2>
2017-11-05 17:43:18 +0900

    Episode 16

---
+++ my-app/src/components/Message.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box is-info">
+  	<p>
+    	<slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+	.box { background: #e3e3e3; }
+</style>
